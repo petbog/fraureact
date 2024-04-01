cc35e67d63923a2d663cead88c57114f4b89fc44
bogdan petrov <bogdan_emdetei_petrov@mail.ru>
2024-04-01 15:28:35 +0300

    first commit

---
+++ src/Globals.d.ts
@@ -0,0 +1,7 @@
+declare module "*.module.css";
+declare module "*.module.scss";
+declare module "*.png"
+declare module "*.svg"
+declare module "*.jpg"
+declare module "*.mp4"
+declare module "*.gif"
